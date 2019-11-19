d38cc922c385e3e37b03f61a35bd5c45a294bb52
Louis <louis.gridley.wu@zoho.eu>
2019-11-19 21:01:51 +0100

    Initial Commit

---
+++ Domain/Value.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Domain
+{
+    public class Value
+    {
+        // primary Key
+        public int Id { get; set; }
+        public string Name{ get; set; }
+        
+    }
+   
+}
